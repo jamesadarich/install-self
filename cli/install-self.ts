d0ddd81b4e0709fe8cc035c385f456247038b5c7
James Richford <jamesrichford@googlemail.com>
2016-12-05 22:46:21 +0000

    plan out CLI process

---
+++ cli/install-self.ts
@@ -0,0 +1,9 @@
+// get package name
+
+// create folder with package name
+
+// create index file to that folder
+// with the following contents
+// module.exports = require("..");
+
+// log errors or log everything's A-OK
